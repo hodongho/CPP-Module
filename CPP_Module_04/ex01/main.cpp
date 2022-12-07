69d7abcda5bb9cb7b81b58751bc59ac1c4cc2c0c
Dongho Hyun <ehdghho@kakao.com>
2022-12-07 10:57:10 +0900

    Fix CPP04 ex01 main.cpp

--- CPP_Module_04/ex01/main.cpp
+++ CPP_Module_04/ex01/main.cpp
@@ -6,15 +6,25 @@
 
 int	main()
 {
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
+	{
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
 
-	std::string dog = "My type is Dog";
-	std::string cat = "My type is Cat";
+		std::string dog = "My type is Dog";
+		std::string cat = "My type is Cat";
 
-	delete j;
-	delete i;
-
-	std::cout << std::endl << "---------------------------------------------" << std::endl << std::endl;
+		delete j;
+		delete i;
+	}
+	{
+		Animal* Animals[4];
+		for (int i = 0; i < 3; i++)
+		{
+			if (i < 1)
+				Animals[i] = new Dog();
+			else
+				Animals[i] = new Cat();
+		}
+	}
 	return (0);
 }
